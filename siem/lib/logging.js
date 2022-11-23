acebed5ae1d06ca2c2d492c9866fc35685af0718
DepressedFruit <118745329+DepressedFruit@users.noreply.github.com>
2022-11-23 14:48:34 +0000

    Build: (3ce6b31) Revert "Added prefix and some changes."
    This reverts commit b3842653b278b12b5257494f560d04f80472fec6.

--- siem/lib/logging.js
+++ siem/lib/logging.js
@@ -16,53 +16,48 @@ const fs_1 = require("fs");
 const promises_1 = require("fs/promises");
 const path_1 = require("path");
 const dayjs_1 = __importDefault(require("dayjs"));
-const INFO = console.info;
-const DEBUG = console.debug;
-const WARN = console.warn;
-const ERROR = console.error;
-function default_1(mode, prefix = null) {
+function default_1(mode) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const LOG_DIR = (0, path_1.join)(__dirname, '../', '../', 'logs');
             const LOG_FILE = (0, path_1.join)(__dirname, '../', '../', 'logs', `${mode}.log`);
-            if (!(0, fs_1.existsSync)(LOG_DIR))
+            if (!(0, fs_1.existsSync)(LOG_DIR)) {
                 (0, fs_1.mkdirSync)(LOG_DIR);
-            if (!(0, fs_1.existsSync)(LOG_FILE))
-                (0, fs_1.writeFile)(LOG_FILE, 'LOG FILE CREATED', { flag: 'wx' }, () => { });
+            }
+            if (!(0, fs_1.existsSync)(LOG_FILE)) {
+                // Create file.
+                (0, fs_1.writeFile)(LOG_FILE, 'LOG FILE CREATED\n', { flag: 'wx' }, () => { });
+            }
             function appendToLog(type, log) {
                 return __awaiter(this, void 0, void 0, function* () {
                     const currentDateTime = (0, dayjs_1.default)();
-                    let fullLog = `${currentDateTime.format('MM/DD/YYYY HH:mm:ss')} [${type.toUpperCase()}]`;
-                    if (prefix)
-                        fullLog += ` [${prefix}]`;
-                    fullLog += ` ${log}`;
-                    yield (0, promises_1.appendFile)(LOG_FILE, `\n${fullLog}`);
-                    return fullLog;
+                    const fullLog = `${currentDateTime.format('MM/DD/YYYY HH:mm:ss')} [${type.toUpperCase()}] ${log}`;
+                    yield (0, promises_1.appendFile)(LOG_FILE, `${fullLog}\n`);
+                    console.log(fullLog);
                 });
             }
             return {
                 info: (log) => {
-                    appendToLog('info', log).then(fullLog => INFO(fullLog));
+                    appendToLog('info', log).then(() => { });
                 },
                 debug: (log) => {
-                    appendToLog('debug', log).then(fullLog => DEBUG(fullLog));
+                    appendToLog('debug', log).then(() => { });
                 },
-                warn: (log) => {
-                    appendToLog('warning', log).then(fullLog => WARN(fullLog));
+                warning: (log) => {
+                    appendToLog('warning', log).then(() => { });
                 },
                 error: (log) => {
-                    appendToLog('error', log).then(fullLog => ERROR(fullLog));
+                    appendToLog('error', log).then(() => { });
                 },
             };
         }
         catch (err) {
-            const currentDateTime = (0, dayjs_1.default)();
-            ERROR(`${err}`);
+            console.error(`${err}`);
             return {
-                info: (log) => INFO(`${currentDateTime.format('MM/DD/YYYY HH:mm:ss')} [INFO] ${log}`),
-                debug: (log) => DEBUG(`${currentDateTime.format('MM/DD/YYYY HH:mm:ss')} [DEBUG] ${log}`),
-                warn: (log) => WARN(`${currentDateTime.format('MM/DD/YYYY HH:mm:ss')} [WARNING] ${log}`),
-                error: (log) => ERROR(`${currentDateTime.format('MM/DD/YYYY HH:mm:ss')} [ERROR] ${log}`),
+                info: (log) => { },
+                debug: (log) => { },
+                warning: (log) => { },
+                error: (log) => { },
             };
         }
     });
